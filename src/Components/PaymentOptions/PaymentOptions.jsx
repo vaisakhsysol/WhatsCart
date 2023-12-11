59492e3751bd1eccd57c42ec6a16c3cd282e0f17
vaisakhp98 <vaisakhp43@gmail.com>
2023-12-11 16:22:22 +0530

    updated on 11/12/23 Delivery Page COmpleted

--- src/Components/PaymentOptions/PaymentOptions.jsx
+++ src/Components/PaymentOptions/PaymentOptions.jsx
@@ -1,23 +1,77 @@
-import React from "react"
-import './PaymentOptions.css'
-import Collapsible from 'react-collapsible';
+import React, { useState } from "react";
+import './PaymentOptions.css';
+import { FaCaretDown } from "react-icons/fa";
+import CartBill from "../CartBill/CartBill";
 
 
-const PaymentOptions = (props) => {
+const PaymentOptions = () => {
+  const [collapsibles, setCollapsibles] = useState({
+    collapsible1: true,
+    collapsible2: true,
+    collapsible3: true,
+  });
+
+  const toggleCollapse = (collapsibleId) => {
+    setCollapsibles({
+      ...collapsibles,
+      [collapsibleId]: !collapsibles[collapsibleId],
+    });
+  };
+
   return (
     <div className="paymentOtionsContainer">
-        <h2>PAYMENT OPTIONS</h2>
+      <h2>PAYMENT OPTIONS</h2>
 
-        <div className="paymentMethodsContainer">
-            <h5> SELECT PAYMENT METHOD </h5>
+      <div className="paymentMethodsContainer">
+        <div className="paymentMethod">
+            <h5 onClick={() => toggleCollapse('collapsible1')} style={{ cursor: "pointer",
 
+                                                                    padding:"20px" }}>UPI TRANSACTION 
+            <FaCaretDown />
+
+        </h5>
+        {!collapsibles.collapsible1 && (
+          <div className="collapsibleContent">
+            <p>Content for collapsible 1...</p>
+          </div>
+        )}
+        </div>
+        
             
-            
+        <div className="paymentMethod">
+            <h5 onClick={() => toggleCollapse('collapsible2')} style={{ cursor: "pointer",
+                                                                    // border:"1px solid rgb(215, 215, 215)",
+                                                                    padding:"20px" }}>DEBIT/CREDIT CARD 
+            <FaCaretDown />
+
+        </h5>
+        {!collapsibles.collapsible2 && (
+          <div className="collapsibleContent">
+            <p>Content for collapsible 2...</p>
+          </div>
+        )}
         </div>
 
+        <div className="paymentMethod">
+            <h5 onClick={() => toggleCollapse('collapsible3')} style={{ cursor: "pointer",
+                                                                    // border:"1px solid rgb(215, 215, 215)",
+                                                                    padding:"20px" }}>CASH ON DELIVERY 
+            <FaCaretDown />
 
+        </h5>
+        {!collapsibles.collapsible3 && (
+          <div className="collapsibleContent">
+            <p>Content for collapsible 3...</p>
+          </div>
+        )}
+        </div>
+      </div>
+
+      <CartBill/>
+
+      <button className="payNowBtn"> Pay Now </button>
     </div>
-  )
+  );
 };
 
 export default PaymentOptions;
