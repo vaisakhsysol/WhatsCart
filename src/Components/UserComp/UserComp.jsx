17180837e4407a899da30e888a750b831247bc76
vaisakhp98 <vaisakhp43@gmail.com>
2023-12-12 17:25:17 +0530

    updated 12/12/23 user page completed

--- src/Components/UserComp/UserComp.jsx
+++ src/Components/UserComp/UserComp.jsx
@@ -44,14 +44,54 @@ const UserComp = (props) => {
         </div>
 
         <div className="detailsContainerMain">
-            <div>
+            <div className="detailsContentContainer">
                 <div className="detailsHeader">
                     <h3>Personal Details</h3>
                     <button>  <MdOutlineLogout /> Log Out </button> 
                 </div>
 
                 <div>
+                    <div style={{display:"flex"}} >
+                        <div style={{display:"flex",flexDirection:"column"}}>
+                            <label htmlFor="">Name</label>
+                            <p className="titleText">Lorem Ipsum User Name</p>
+                            {/* <input style={{padding:"10px"}} type="text" name="" id="" />  */}
+                        </div>
+                        <div>
+                            <button>
+                                <LiaEditSolid/>
+                            </button>
+                        </div>
+                       
+                    </div>
 
+                    <div style={{display:"flex"}} >
+                        <div style={{display:"flex",flexDirection:"column"}}>
+                            <label htmlFor="">Mobile Number</label>
+                            <p className="titleText">+91 6520 65065 </p>
+                            {/* <input style={{padding:"10px"}} type="text" name="" id="" />  */}
+                        </div>
+                        <div>
+                            <button>
+                                <LiaEditSolid/>
+                            </button>
+                        </div>
+                       
+                    </div>
+
+                    <div style={{display:"flex"}} >
+                        <div style={{display:"flex",flexDirection:"column"}}>
+                            <label htmlFor="">Email</label>
+                            <p className="titleText">loreipsum@gmail.com </p>
+                            {/* <input style={{padding:"10px"}} type="text" name="" id="" />  */}
+                        </div>
+                        <div>
+                            <button>
+                                <LiaEditSolid/>
+                            </button>
+                        </div>
+                       
+                    </div>
                 </div>
             </div>
         </div>
