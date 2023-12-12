7ca56d116eceb9d70064822a78ed3837d5c77a6c
vaisakhp98 <vaisakhp43@gmail.com>
2023-12-12 12:31:53 +0530

    updated 12/12/23 signup  page completed

--- src/Components/LoginComp/LoginComp.jsx
+++ src/Components/LoginComp/LoginComp.jsx
@@ -1,56 +1,86 @@
-import React from "react"
-import './LoginComp.css'
-import loginImg from '../Assets/Images/loginImage.jpg'
+import React, { useState } from "react";
+import './LoginComp.css';
+import loginImg from '../Assets/Images/loginImage.jpg';
 import { Link } from "react-router-dom";
 
 const LoginComp = (props) => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [errors, setErrors] = useState({});
+
+  const handleValidation = () => {
+    let formIsValid = true;
+    let newErrors = {};
+
+    if (!username) {
+      formIsValid = false;
+      newErrors['username'] = 'Username is required';
+    }
+
+    if (!password) {
+      formIsValid = false;
+      newErrors['password'] = 'Password is required';
+    }
+
+    setErrors(newErrors);
+    return formIsValid;
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (handleValidation()) {
+      console.log('Form submitted!');
+    } else {
+      console.log('Form has errors');
+    }
+  };
+
   return (
     <div className="loginContainer">
-        <div className="container">
-            <div className="imageContainer"> 
-                <img src={loginImg} alt="" />
-             </div>
-            <div className="inputsContianer"> 
-                <div>
-                    <h2>Welcome Back User</h2>
-                </div>
-
-                <div className="inputsCredentaialsContainer">
-                    <div className="usernameAndPasswordContaienr">
-                        <label htmlFor="">UserName or Email </label>
-                        <input type="text" placeholder="" />
-                    </div>
-                    <div className="passwordContainer">
-                        <label htmlFor="">Password </label>
-                        <input type="password" placeholder="" />
-                    </div>
-
-                    <div className="buttonsContainer">
-                        
-                        <div className="credButtonsContainer">
-                            <button  className="loginBtn"> Login </button>
-                            <button  className="forgotBtn"> Forgot Password ? Click Here </button>
-
-                        </div>
-                       
-                        <h5>Dont have an Account ?</h5>
-                        <Link to={`/signup/`}>
-                         <button  className="signup"> Sign Up Now </button>
-
-                        </Link>
-                    </div>
-
-                    <div>
-                        {/* <hr/> */}
-                        
-                    </div>
-                    
-                </div>
-            </div>
+      <div className="container">
+        <div className="imageContainer"> 
+          <img src={loginImg} alt="" />
         </div>
-      
+        <div className="inputsContianer"> 
+          <div>
+            <h2>Welcome Back User</h2>
+          </div>
+          <form className="inputsCredentaialsContainer" onSubmit={handleSubmit}>
+            <div className="usernameAndPasswordContaienr">
+              <label htmlFor="username">UserName or Email </label>
+              <input
+                type="text"
+                placeholder=""
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+              />
+              {errors.username && <span style={{color:"red" , marginTop:" 4px"}} className="error">{errors.username}</span>}
+            </div>
+            <div className="passwordContainer">
+              <label htmlFor="password">Password </label>
+              <input
+                type="password"
+                placeholder=""
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              {errors.password && <span style={{color:"red" , marginTop:" 4px"}} className="error">{errors.password}</span>}
+            </div>
+            <div className="buttonsContainer">
+              <div className="credButtonsContainer">
+                <button className="loginBtn" type="submit"> Login </button>
+                <button className="forgotBtn"> Forgot Password ? Click Here </button>
+              </div>
+              <h5>Dont have an Account ?</h5>
+              <Link to={`/signup/`}>
+                <button className="signup"> Sign Up Now </button>
+              </Link>
+            </div>
+          </form>
+        </div>
+      </div>
     </div>
-  )
+  );
 };
 
 export default LoginComp;
--- src/Components/SignUpComp/SignUpComp.jsx
+++ src/Components/SignUpComp/SignUpComp.jsx
@@ -1,60 +1,124 @@
-import React from "react"
-import './SignUpComp.css'
-import signUpImg from '../Assets/Images/signUp.jpg'
+import React, { useState } from "react";
+import './SignUpComp.css';
+import signUpImg from '../Assets/Images/signUp.jpg';
 import { Link } from "react-router-dom";
 
 const SignUpComp = (props) => {
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [errors, setErrors] = useState({});
+
+  const handleValidation = () => {
+    let formIsValid = true;
+    let newErrors = {};
+
+    if (!firstName) {
+      formIsValid = false;
+      newErrors['firstName'] = 'First Name is required';
+    }
+
+    if (!lastName) {
+      formIsValid = false;
+      newErrors['lastName'] = 'Last Name is required';
+    }
+
+    if (!email) {
+      formIsValid = false;
+      newErrors['email'] = 'Email is required';
+    } else {
+      // Basic email validation
+      const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+      if (!emailPattern.test(email)) {
+        formIsValid = false;
+        newErrors['email'] = 'Invalid email address';
+      }
+    }
+
+    if (!password) {
+      formIsValid = false;
+      newErrors['password'] = 'Password is required';
+    }
+
+    setErrors(newErrors);
+    return formIsValid;
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (handleValidation()) {
+    
+      console.log('Form submitted!');
+    } else {
+      console.log('Form has errors');
+    }
+  };
+
   return (
-    <div>
-      <div className="signpContainer">
-        <div className="container">
-
-            <div>
-                <div className="imageContainer">
-                    <img src={signUpImg} alt="" />
-                </div>
-            </div>
-            <div className="inputsContainer">
-                <h3>Hi There User , Nice to Meet You</h3>
-
-                <div className="inputsContainer">
-
-                    <div >
-                        <div className="usernameAndEmailContainer">
-                            <label htmlFor=""> First Name</label>
-                            <input type="text" name="" id="" />
-                        </div>
-                        <div className="usernameAndEmailContainer">
-                            <label htmlFor=""> Last Name</label>
-                            <input type="text" name="" id="" />
-                        </div>
-                        <div className="usernameAndEmailContainer">
-                            <label htmlFor=""> Email</label>
-                            <input type="email" name="" id="" />
-                        </div>
-                        <div className="usernameAndEmailContainer">
-                            <label htmlFor=""> Password </label>
-                            <input type="password" name="" id="" />
-                        </div>
-                        
-                    </div>
-
-                    <div>
-                        <button className="signUpBtn">Sign Up</button>
-                        <Link to={`/login/`}>
-                            <button className="loginBtn">Already have an Account ? Login</button>
-                        </Link>
-                    </div>
-
-
-                </div>
-            </div>
-
+    <div className="signpContainer">
+      <div className="container">
+        <div>
+          <div className="imageContainer">
+            <img src={signUpImg} alt="" />
+          </div>
         </div>
-        
+        <div className="inputsContainer">
+          <h3>Hi There User, Nice to Meet You</h3>
+          <form onSubmit={handleSubmit}>
+            <div className="inputsContainer">
+              <div className="usernameAndEmailContainer">
+                <label htmlFor="firstName"> First Name</label>
+                <input
+                  type="text"
+                  id="firstName"
+                  value={firstName}
+                  onChange={(e) => setFirstName(e.target.value)}
+                />
+                {errors.firstName && <span style={{color:"red" , marginTop:" 4px"}} className="error">{errors.firstName}</span>}
+              </div>
+              <div className="usernameAndEmailContainer">
+                <label htmlFor="lastName"> Last Name</label>
+                <input
+                  type="text"
+                  id="lastName"
+                  value={lastName}
+                  onChange={(e) => setLastName(e.target.value)}
+                />
+                {errors.lastName && <span style={{color:"red" , marginTop:" 4px"}} className="error">{errors.lastName}</span>}
+              </div>
+              <div className="usernameAndEmailContainer">
+                <label htmlFor="email"> Email</label>
+                <input
+                  type="email"
+                  id="email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+                {errors.email && <span style={{color:"red" , marginTop:" 4px"}} className="error">{errors.email}</span>}
+              </div>
+              <div className="usernameAndEmailContainer">
+                <label htmlFor="password"> Password </label>
+                <input
+                  type="password"
+                  id="password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+                {errors.password && <span style={{color:"red" , marginTop:" 4px"}} className="error">{errors.password}</span>}
+              </div>
+            </div>
+            <div>
+              <button className="signUpBtn" type="submit">Sign Up</button>
+              <Link to={`/login/`}>
+                <button className="loginBtn">Already have an Account ? Login</button>
+              </Link>
+            </div>
+          </form>
+        </div>
       </div>
     </div>
-  )
+  );
 };
 
 export default SignUpComp;
