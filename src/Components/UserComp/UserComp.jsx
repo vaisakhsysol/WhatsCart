73d8297393b569056d5c014280afff408d3d6354
vaisakhp98 <vaisakhp43@gmail.com>
2023-12-13 11:26:54 +0530

    updated 12/12/23 user page WokINProg

--- src/Components/UserComp/UserComp.jsx
+++ src/Components/UserComp/UserComp.jsx
@@ -24,7 +24,7 @@ const UserComp = (props) => {
   return (
     <div className="userContainer">
         
-        <div >
+        <div className="userTabButtons">
             <button className="tabButton">
                 <LiaUser className="userIcon"/>
                 <div className="detailsContainer">
