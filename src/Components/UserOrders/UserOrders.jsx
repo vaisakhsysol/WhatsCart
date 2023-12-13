e696e0c108dd45a33dd8732f60b6f1c51e5b9406
vaisakhp98 <vaisakhp43@gmail.com>
2023-12-13 15:14:40 +0530

    updated 12/12/23 user page WokINProg

--- src/Components/UserOrders/UserOrders.jsx
+++ src/Components/UserOrders/UserOrders.jsx
@@ -15,21 +15,87 @@ const UserOrders = () => {
                 <div className="imageContainer">
                   <img src={laptopOne} alt="" />
 
-                  <div>
-                    <p>Name</p>
-                    <p>Desc</p>
-                    <p>Price</p>
-                    <p>Unit</p>
+                  
                 </div>
+
+                <div className="productDetails">
+                    <p className="productName">Lenovo Legion 3000x</p>
+                    {/* <p className="productDesc">Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias minus facilis id nesciunt, voluptatum laboriosam odio debitis sapiente voluptatem ipsum? Molestias, qui! Ullam error, doloremque nam molestias minus vero eaque.</p> */}
+                    <p className="productPrice">Rs.32,000</p>
+                    <p className="productUnit">2 Nos</p>
                 </div>
+                
 
                 
 
             </div>
             
-            <div>
-                <p>Delivery Date</p>
+            <div className="statusContainer">
+              <div style={{backgroundColor:"green"}} className="sphere"></div>
+              <div className="dateAndStatus">
+                <p> Delivered on 23/11/2023 </p>
             </div>
+        </div>
+
+        </div>
+
+        <div className="productOrdersContainer">
+            <div className="productOrderDetails">
+
+                <div className="imageContainer">
+                  <img src={laptopOne} alt="" />
+
+                  
+                </div>
+
+                <div className="productDetails">
+                    <p className="productName">Lenovo Legion 3000x</p>
+                    {/* <p className="productDesc">Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias minus facilis id nesciunt, voluptatum laboriosam odio debitis sapiente voluptatem ipsum? Molestias, qui! Ullam error, doloremque nam molestias minus vero eaque.</p> */}
+                    <p className="productPrice">Rs.32,000</p>
+                    <p className="productUnit">2 Nos</p>
+                </div>
+                
+
+                
+
+            </div>
+            
+            <div className="statusContainer">
+              <div style={{background:"orange"}} className="sphere"></div>
+              <div style={{color:"orange"}} className="dateAndStatus">
+                <p> Refunded on 23/11/2023 </p>
+            </div>
+        </div>
+
+        </div>
+        <div className="productOrdersContainer">
+            <div className="productOrderDetails">
+
+                <div className="imageContainer">
+                  <img src={laptopOne} alt="" />
+
+                  
+                </div>
+
+                <div className="productDetails">
+                    <p className="productName">Lenovo Legion 3000x</p>
+                    {/* <p className="productDesc">Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias minus facilis id nesciunt, voluptatum laboriosam odio debitis sapiente voluptatem ipsum? Molestias, qui! Ullam error, doloremque nam molestias minus vero eaque.</p> */}
+                    <p className="productPrice">Rs.32,000</p>
+                    <p className="productUnit">2 Nos</p>
+                </div>
+                
+
+                
+
+            </div>
+            
+            <div  className="statusContainer">
+              <div style={{backgroundColor:"red"}} className="sphere"></div>
+              <div style={{color:"red"}} className="dateAndStatus">
+                <p> Cancelled on 23/11/2023 </p>
+            </div>
+        </div>
+
         </div>
       </div>
     </div>
