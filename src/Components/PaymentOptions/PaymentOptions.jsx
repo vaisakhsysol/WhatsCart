fdd19082991c97ee02e59145044c44210a4b3425
vaisakhp98 <vaisakhp43@gmail.com>
2023-12-23 16:54:50 +0530

    UI updates

--- src/Components/PaymentOptions/PaymentOptions.jsx
+++ src/Components/PaymentOptions/PaymentOptions.jsx
@@ -1,78 +1,71 @@
 import React, { useState } from "react";
 import './PaymentOptions.css';
-import { FaCaretDown } from "react-icons/fa";
 import CartBill from "../CartBill/CartBill";
 import { Link } from "react-router-dom";
-
+import { MdQrCodeScanner } from "react-icons/md";
+import { IoMdCard } from "react-icons/io";
+import { TbTruckDelivery } from "react-icons/tb";
 
 const PaymentOptions = () => {
-  const [collapsibles, setCollapsibles] = useState({
-    collapsible1: true,
-    collapsible2: true,
-    collapsible3: true,
-  });
+  const [showUpiPayment, setShowUpiPayment] = useState(false);
+  const [showDebitOrCredit, setShowDebitOrCredit] = useState(false);
+  const [showCashOnDelivery, setShowCashOnDelivery] = useState(false);
 
-  const toggleCollapse = (collapsibleId) => {
-    setCollapsibles({
-      ...collapsibles,
-      [collapsibleId]: !collapsibles[collapsibleId],
-    });
+  const handleUpiPaymentClick = () => {
+    setShowUpiPayment(true); 
+    setShowDebitOrCredit(false);
+    setShowCashOnDelivery(false);
+  };
+
+  const handleDebitOrCreditClick = () => {
+    setShowUpiPayment(false);
+    setShowDebitOrCredit(true);
+    setShowCashOnDelivery(false);
+  };
+
+  const handleCashOnDeliveryClick = () => {
+    setShowUpiPayment(false);
+    setShowDebitOrCredit(false);
+    setShowCashOnDelivery(true);
   };
 
   return (
     <div className="paymentOtionsContainer">
       <h2>PAYMENT OPTIONS</h2>
+      <div className="paymentOptionsContainerTabMain">
+        <div className="cartPaymentTabButtons">
+          <button className="paymentOptionsButtons" onClick={handleUpiPaymentClick}>
+            <MdQrCodeScanner style={{ marginRight: "12px" }} />
+            UPI Payments 
+          </button>
 
-      <div className="paymentMethodsContainer">
-        <div className="paymentMethod">
-            <h5 onClick={() => toggleCollapse('collapsible1')} style={{ cursor: "pointer",
+          <button style={{ marginTop: "10px" }} className="paymentOptionsButtons" onClick={handleDebitOrCreditClick}>
+            <IoMdCard style={{ marginRight: "12px" }}/>
+            Debit or Credit
+          </button>
 
-                                                                    padding:"20px" }}>UPI TRANSACTION 
-            <FaCaretDown />
-
-        </h5>
-        {!collapsibles.collapsible1 && (
-          <div className="collapsibleContent">
-            <p>Content for collapsible 1...</p>
-          </div>
-        )}
+          <button style={{ marginTop: "10px" }} className="paymentOptionsButtons" onClick={handleCashOnDeliveryClick}>
+            <TbTruckDelivery style={{ marginRight: "12px" }}/>
+            Cash on Delivery 
+          </button>
         </div>
-        
-            
-        <div className="paymentMethod">
-            <h5 onClick={() => toggleCollapse('collapsible2')} style={{ cursor: "pointer",
-                                                                    // border:"1px solid rgb(215, 215, 215)",
-                                                                    padding:"20px" }}>DEBIT/CREDIT CARD 
-            <FaCaretDown />
-
-        </h5>
-        {!collapsibles.collapsible2 && (
-          <div className="collapsibleContent">
-            <p>Content for collapsible 2...</p>
+        <div>
+          <div className={showUpiPayment ? "upiPayment visible" : "upiPayment"}>
+            Upi Payment
           </div>
-        )}
-        </div>
-
-        <div className="paymentMethod">
-            <h5 onClick={() => toggleCollapse('collapsible3')} style={{ cursor: "pointer",
-                                                                    // border:"1px solid rgb(215, 215, 215)",
-                                                                    padding:"20px" }}>CASH ON DELIVERY 
-            <FaCaretDown />
-
-        </h5>
-        {!collapsibles.collapsible3 && (
-          <div className="collapsibleContent">
-            <p>Content for collapsible 3...</p>
+          <div className={showDebitOrCredit ? "debitOrCredit visible" : "debitOrCredit"}>
+            Debit or Credit
           </div>
-        )}
+          <div className={showCashOnDelivery ? "cashOnDelivery visible" : "cashOnDelivery"}>
+            Cash on Delivery
+          </div>
         </div>
       </div>
-
+    
       <CartBill/>
 
       <Link to={`/success/`}>
         <button className="payNowBtn"> Pay Now </button>
-
       </Link>
     </div>
   );
